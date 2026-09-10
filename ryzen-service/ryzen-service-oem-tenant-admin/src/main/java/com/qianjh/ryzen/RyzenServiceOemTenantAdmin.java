9871dfe0918d5a09d576b24100d5c52f700621e1
QianJH <qianjh@qq.com>
2026-09-10 17:53:36 +0800

    init

---
+++ ryzen-service/ryzen-service-oem-tenant-admin/src/main/java/com/qianjh/ryzen/RyzenServiceOemTenantAdmin.java
@@ -0,0 +1,16 @@
+package com.qianjh.ryzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author QianJH
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RyzenServiceOemTenantAdmin {
+    public static void main(String[] args) {
+        SpringApplication.run(RyzenServiceOemTenantAdmin.class, args);
+    }
+}
