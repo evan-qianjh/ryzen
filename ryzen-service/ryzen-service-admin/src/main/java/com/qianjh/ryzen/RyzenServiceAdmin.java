5f4ab2e8a0a369c77cc643dfc5f3ecc7e995ed1c
QianJH <qianjh@qq.com>
2026-09-10 20:52:51 +0800

    init

--- ryzen-service/ryzen-service-admin/src/main/java/com/qianjh/ryzen/RyzenServiceAdmin.java
+++ ryzen-service/ryzen-service-admin/src/main/java/com/qianjh/ryzen/RyzenServiceAdmin.java
@@ -9,8 +9,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class RyzenServiceOemTenantAdmin {
+public class RyzenServiceAdmin {
     public static void main(String[] args) {
-        SpringApplication.run(RyzenServiceOemTenantAdmin.class, args);
+        SpringApplication.run(RyzenServiceAdmin.class, args);
     }
 }
